bce921316be82e3b522ccafbbf30360f97fe3580
Mahnaz Mahmoodzadeh <mahnaz.mahmoodzade@gmail.com>
2020-12-10 01:35:33 +0330

    Cache products to prevent requsting products when change the view

--- src/app/shared/api-services/product.service.ts
+++ src/app/shared/api-services/product.service.ts
@@ -1,17 +1,25 @@
-import {Observable} from "rxjs";
+import {Observable, of} from "rxjs";
 import {ProductModel} from "../models/product.model";
 import {ApiService} from "./api.service";
 import {Injectable} from "@angular/core";
+import {catchError, map} from "rxjs/operators";
 
 @Injectable()
 export class ProductService {
   basePath = 'products';
+  cache: ProductModel[];
 
   constructor(private apiService: ApiService) {
   }
 
   getAll(): Observable<ProductModel[]> {
-    return this.apiService.get(this.basePath);
+    if(this.cache){
+      return of(this.cache);
+    }
+    return this.apiService.get(this.basePath).pipe(map((res: any) => {
+        this.cache=res;
+        return res;
+      }));
   }
 
 
